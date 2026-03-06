2873ccb71838fc97e1b2b9f6abeccb5c00f6f2fa
r-zubkov <r.zubkov.it@gmail.com>
2026-03-06 14:37:27 +0300

    removed legacy

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { provideEventPlugins } from '@taiga-ui/event-plugins';
 import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [
-    ...(appConfig.providers || []),
-    provideHttpClient(),
-    provideAnimations(),
-    provideEventPlugins(),
-  ],
+  providers: [...(appConfig.providers || []), provideHttpClient(), provideEventPlugins()],
 }).catch((err) => console.error(err));
