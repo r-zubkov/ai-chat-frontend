1d8f898d106094bc97a14120e84d677cafba4788
r-zubkov <r.zubkov.it@gmail.com>
2026-03-15 17:25:07 +0300

    added pwa support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,19 @@
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import { MigrationService } from '@shared/db';
+import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes, withComponentInputBinding()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
     provideAppInitializer(() => {
       const migrationService = inject(MigrationService);
       return migrationService.migrateIfNeeded();
